ec070c58f96c7180dc6360b99723fddd2ff1e61a
Genadijs Zuravlovs <gen.zuravlov@gmail.com>
2018-02-16 22:54:05 +0200

    dsa

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ const next = document.querySelector("#next");
 const items = document.querySelector("#items");
 
 const minRight = 0;
-const maxRight = 2880;
+const maxRight = 1920;
 const step = 960;
 let currentRight = 0;
 
-prev.addEventListener("click", function() {
+next.addEventListener("click", function() {
     if (currentRight < maxRight) {
       currentRight += step;
       items.style.right = currentRight + "px";
     }
   });
   
-next.addEventListener("click", function() {
+prev.addEventListener("click", function() {
     if (currentRight > minRight) {
       currentRight -= step;
       items.style.right = currentRight + "px";
